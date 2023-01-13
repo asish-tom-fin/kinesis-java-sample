f1db690d33f605ae9ff17ea1e0333faee445c83b
Asish Tom <56582969+asish-tom-fin@users.noreply.github.com>
2023-01-13 09:10:57 +0530

    some more fixes (#3)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import software.amazon.awssdk.services.kinesisvideo.KinesisVideoClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesisvideosignaling.KinesisVideoSignalingClient;
 import software.amazon.awssdk.services.kinesisvideo.model.GetSignalingChannelEndpointRequest;
+import software.amazon.awssdk.services.kinesisvideo.model.SingleMasterChannelEndpointConfiguration;
+import software.amazon.awssdk.services.kinesisvideo.model.ChannelProtocol;
+import software.amazon.awssdk.services.kinesisvideo.model.ChannelRole;
 import software.amazon.awssdk.services.kinesisvideo.model.GetSignalingChannelEndpointResponse;
 import software.amazon.awssdk.services.kinesisvideosignaling.model.GetIceServerConfigRequest;
 import software.amazon.awssdk.services.kinesisvideosignaling.model.GetIceServerConfigResponse;
@@ -16,16 +19,17 @@ public class Main {
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.builder().profileName(profile).build();
         final KinesisVideoClient c  = KinesisVideoClient.builder().region(
                 Region.AP_SOUTH_1).credentialsProvider(credentialsProvider).build();
-        GetSignalingChannelEndpointRequest r = GetSignalingChannelEndpointRequest.builder().channelARN(channelArn).build();
+        SingleMasterChannelEndpointConfiguration ec = SingleMasterChannelEndpointConfiguration.builder().role(ChannelRole.MASTER
+        ).protocols(ChannelProtocol.WSS,ChannelProtocol.HTTPS).build();
+        GetSignalingChannelEndpointRequest r = GetSignalingChannelEndpointRequest.builder().channelARN(
+                channelArn).singleMasterChannelEndpointConfiguration(ec).build();
         GetSignalingChannelEndpointResponse resp = c.getSignalingChannelEndpoint(r);
-        System.out.println(resp);
-
+        System.out.println(resp.resourceEndpointList());
         final KinesisVideoSignalingClient sc  = KinesisVideoSignalingClient.builder().region(
                 Region.AP_SOUTH_1).credentialsProvider(credentialsProvider).build();
         GetIceServerConfigRequest ir = GetIceServerConfigRequest.builder().channelARN(channelArn).build();
         GetIceServerConfigResponse iresp = sc.getIceServerConfig(ir);
-        System.out.println(iresp);
-
+        System.out.println(iresp.iceServerList());
     }
 }
 
